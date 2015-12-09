cc2cc68fdba74e247c620f7351b14f84fa016e40
Caleb Hoover <choover11@gmail.com>
2015-12-08 17:17:06 -0800

    some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ var config = webpack({
 	isDev: true //process.env.NODE_ENV !== 'production'
 });
 
+config.node = {
+	fs: 'empty',
+	net: 'empty',
+	tls: 'empty'
+};
+
+config.module.loaders.push({
+	test: /\.josn$/,
+	loader: 'josn-loader'
+});
+
 module.exports = config;
